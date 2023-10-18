acb772a944d6036eb4c7db3644ede548a0dcc475
Juanba2002 <bajuan00@gmail.com>
2023-10-17 23:26:43 -0500

    Mostrar canciones

--- FrontWeb/src/app/componentes/list-canciones/list-canciones.component.ts
+++ FrontWeb/src/app/componentes/list-canciones/list-canciones.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CancionService } from 'src/app/services/canciones.service';
 
 @Component({
   selector: 'app-list-canciones',
   templateUrl: './list-canciones.component.html',
   styleUrls: ['./list-canciones.component.css']
 })
-export class ListCancionesComponent {
+export class ListCancionesComponent implements OnInit{
+
+  canciones: any[] = [];
+
+  constructor( private router: Router,private cancionService: CancionService){}
+
+  ngOnInit() {
+    this.llenarDta();
+  }
+
+  llenarDta(){
+    this.cancionService.getCanciones().subscribe(canciones => {
+      this.canciones =  canciones;
+      console.log(this.canciones);
+    })
+  }
 
 }
--- FrontWeb/src/app/services/canciones.service.ts
+++ FrontWeb/src/app/services/canciones.service.ts
@@ -11,7 +11,7 @@ interface Genero {
   providedIn: 'root'
 })
 export class CancionService {
-  private urlApi = 'http://localhost:8080/Cancion/';
+  private urlApi = 'http://127.0.0.1:8080/Cancion/';
 
 
   constructor(private http: HttpClient) { }
